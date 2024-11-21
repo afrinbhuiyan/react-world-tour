8a922679ec285f2de9f415f2c0fbde9dcecd06b7
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-21 20:04:22 +0600

    add the conditional style & update state & pass parameter

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
-    const {name, flags, population, area} = country;
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
+    const {name, flags, population, area, cca3} = country;
+
+    const [visited, setvisited] = useState(false);
+
+
+    const handleVisited = () => {
+        setvisited(!visited);
+    }
+
+    // console.log(handleVisitedCountry)
+
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'visited' : 'nonvisited'}`}>
             <h3><span style={{color:"red"}}>Name : </span>{name?.common} </h3>
             <img src={flags.png} alt="" />
             <p>Population : {population}</p>
             <p>Area : {area} </p>
+            <p>Code : {cca3} </p>
+            <button onClick={() => handleVisitedCountry(country)}
+            style={{backgroundColor:"lightcoral", color:'white', marginBottom:"20px"}} >Mark Visited</button><br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)} >add flags</button>
+            <br />
+            <button onClick={handleVisited} > {visited ? "Visited" : "Going"}</button>
+            {visited ? "I have visited this country." : "I want to visit"}
         </div>
     );
 };
